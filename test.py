1630a079c3d1bd0855c5f3318bafbd0bface0b7e
Razorlanca1 <kartem423@gmail.com>
2024-12-21 12:00:38 +0300

    unit tests updated

--- test.py
+++ test.py
@@ -6,26 +6,31 @@ user_id = 0
 def check_auth():
     global cookies
 
-    ans = []
-    ans.append(requests.get("http://127.0.0.3:8002/v1/tasks").status_code)
-    ans.append(requests.get("http://127.0.0.3:8002/v1/task/1").status_code)
-    ans.append(requests.post("http://127.0.0.3:8002/v1/add_task", params={"name": "1", "difficult": 1, "description": "1", "user_id": "1"}).status_code)
-    ans.append(requests.delete("http://127.0.0.3:8002/v1/delete_task/1").status_code)
-    ans.append(requests.get("http://127.0.0.3:8002/v2/users").status_code)
-    ans.append(requests.get("http://127.0.0.3:8002/v2/user/1").status_code)
-    resp = requests.get("http://127.0.0.3:8002/auth")
-    cookies = resp.cookies
-    if resp.status_code == 200 and ans == [401] * len(ans):
+    try:
+        ans = []
+        ans.append(requests.get("http://127.0.0.3:8002/v1/tasks").status_code)
+        ans.append(requests.get("http://127.0.0.3:8002/v1/task/1").status_code)
+        ans.append(requests.post("http://127.0.0.3:8002/v1/add_task", params={"name": "1", "difficult": 1, "description": "1", "user_id": "1"}).status_code)
+        ans.append(requests.delete("http://127.0.0.3:8002/v1/delete_task/1").status_code)
+        ans.append(requests.get("http://127.0.0.3:8002/v2/users").status_code)
+        ans.append(requests.get("http://127.0.0.3:8002/v2/user/1").status_code)
+        resp = requests.get("http://127.0.0.3:8002/auth")
+        cookies = resp.cookies
+        if resp.status_code != 200 or ans != [401] * len(ans):
+            raise Exception
         print("Auth tests success")
-    else:
+    except Exception:
         print("Auth tests failed")
 
 
 def check_v2_service():
-    global cookies, user_id
+    global user_id
 
     try:
         ans = []
+        resp = requests.get("http://127.0.0.3:8002/auth")
+        cookies = resp.cookies
+        ans.append(resp.status_code)
         resp = requests.post("http://127.0.0.3:8002/v2/add_user", params={"name": "1"}, cookies=cookies)
         ans.append(resp.status_code)
         resp = requests.get("http://127.0.0.3:8002/v2/users", cookies=cookies)
@@ -50,10 +55,13 @@ def check_v2_service():
 
 
 def check_v1_service():
-    global cookies, user_id
+    global user_id
 
     try:
         ans = []
+        resp = requests.get("http://127.0.0.3:8002/auth")
+        cookies = resp.cookies
+        ans.append(resp.status_code)
         resp = requests.post("http://127.0.0.3:8002/v1/add_task", params={"name": "1", "difficult": 1,
                                                      "description": "1", "user_id": user_id}, cookies=cookies)
         ans.append(resp.status_code)
